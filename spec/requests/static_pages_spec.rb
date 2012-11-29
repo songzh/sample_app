68ce991cddec75dfcd20eac38502856c440441c2
songzh <zangsong@hotmail.com>
2012-11-29 21:59:31 +0800

    create contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+  
   describe "GET /static_pages" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -14,20 +16,20 @@ describe "StaticPages" do
     it "should have the content, 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
     end
 
     it "should have the right title 'Home'" do
       visit "/static_pages/home"
       page.should have_selector('title', 
-        :text => "Ruby on Rails Tutorial Sample App | Home")
+        :text => "#{base_title} | Home")
       end
   end
   
   describe "help Page" do
     it "should have the content, 'Help'" do
       visit "/static_pages/help"
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the right title 'Help'" do
@@ -40,13 +42,21 @@ describe "StaticPages" do
   describe "About Page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the right title 'About US'" do
       visit "/static_pages/about"
       page.should have_selector('title', 
         :text => "Ruby on Rails Tutorial Sample App | About")
+      end
+  end
+  
+  describe "Contact Page" do
+    it "should have the right title 'Contact'" do
+      visit "/static_pages/contact"
+      page.should have_selector('title', 
+        :text => "Ruby on Rails Tutorial Sample App | Contact")
       end
   end
   
